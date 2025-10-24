758bee0c389aa987308e81589fe2dad780eeeded
dvp-coms <dtv12345@hotmail.com>
2025-10-24 14:38:33 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/with-contenv bashio
-npm i
-npm run dev
+ Get the image name from add-on options, or use a default
+IMAGE=${ADDON_CONFIG_IMAGE:-"radrootllc/karaoke-eternal/v1.0.0:latest"}
+
+# Pull the Docker image
+echo "Pulling Docker image: $IMAGE"
+docker pull "$IMAGE"
+
+# Start the Docker container
+echo "Starting Docker container for image: $IMAGE"
+docker run -d --name karaokeeternalHA --restart unless-stopped "$IMAGE"
